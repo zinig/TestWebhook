74232e722758254a62079d14f759e590d735fcd7
GiancarloZini <Giancarlo.zini@nttdata.com>
2021-06-09 13:06:13 +0200

    Added simple console applicaizone

---
+++ JenkinsConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jenkins Hello World !");
+        }
+    }
+}
